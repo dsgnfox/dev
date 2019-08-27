7b584d4b17a8978a3f06d311fd28db0db4159b2a
Artem <dsgnfox@gmail.com>
2019-08-27 16:05:02 +0300

    knyazev-grad start

---
+++ knyazev-grad/gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps'),
+    concat = require('gulp-concat'),
+    browserSync = require('browser-sync').create();
+
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./public"
+        }
+    });
+    browserSync.watch('public', browserSync.reload)
+});
+
+
+gulp.task('style', function () {
+   return gulp.src([
+    './src/sass/**/*.sass'
+    ])
+   .pipe(sourcemaps.init())
+   .pipe(sass({outputStyle: 'expanded'}))
+   .pipe(autoprefixer({
+            overrideBrowserslist: ['last 10 versions'],
+            cascade: false
+        }))
+   .pipe(concat('main.css'))
+   .pipe(sourcemaps.write())
+   .pipe(gulp.dest('./public/css'))
+});
+
+gulp.task('bs', function () {
+   return gulp.src([
+    './src/sass/bootstap/**/*.scss'
+    ])
+   .pipe(sass({outputStyle: 'expanded'}))
+   .pipe(autoprefixer({
+            overrideBrowserslist: ['last 10 versions'],
+            cascade: false
+        }))
+   .pipe(concat('bootstrap.css'))
+   .pipe(gulp.dest('./public/css'))
+});
+
+
+gulp.task('watch', function () {
+    gulp.watch('./src/sass/**/*.sass', gulp.series('style'));
+    gulp.watch('./public/*.html', browserSync.reload);
+});
+
+
+gulp.task('default', gulp.series (
+    gulp.parallel('style'),
+    gulp.parallel('watch', 'browser-sync')
+));
